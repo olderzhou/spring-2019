ca909db157f670698b203ad4e5788f55b097e420
klaus mikaelson <zhouduqing@stargraph.cn>
2019-12-10 17:57:43 +0800

    feature 1. test es

--- spring-digger/spring-elasticsearch/src/main/java/com/klaus/interview/springelasticsearch/model/SchoolPo.java
+++ spring-digger/spring-elasticsearch/src/main/java/com/klaus/interview/springelasticsearch/model/SchoolPo.java
@@ -1,6 +1,8 @@
 package com.klaus.interview.springelasticsearch.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import javax.annotation.Generated;
 import java.util.Date;
 
 @Data
@@ -18,25 +21,35 @@ import java.util.Date;
 @NoArgsConstructor
 @Document(indexName = "spring-elastic-test", type = "school", createIndex = false)
 @ToString
+@ApiModel(value = "学校", description = "school entity description")
 public class SchoolPo {
-    @Id
-    private Long id;
+    @Id()
+    @ApiModelProperty(name = "主键",example = "1")
+    private String id;
+
     @Field(analyzer="ik_max_word", type = FieldType.Text)
+    @ApiModelProperty(name = "名称",example = "klaus")
     private String name;
 
     @Field(type = FieldType.Keyword)
+    @ApiModelProperty(name = "编码",example = "2019111022")
     private String code;
 
-    @Field(type = FieldType.Date, format = DateFormat.basic_date)
+    @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss || yyyy-MM-dd || yyyy/MM/dd HH:mm:ss|| yyyy/MM/dd ||epoch_millis")
     @JsonFormat(pattern = "yyyyMMdd")
+    @ApiModelProperty(name = "建立日",example = "2019-12-10")
     private Date foundDate = new Date();
 
-    @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
-    @JsonFormat(pattern = "yyyyMMdd'T'HHmmss.SSSZ")
-    private Date birth_day;
+    @Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss || yyyy-MM-dd || yyyy/MM/dd HH:mm:ss|| yyyy/MM/dd ||epoch_millis")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(name = "建立时间",example = "2019-12-10 11:51:30")
+    private Date birth_day = new Date();
 
     @Field(type = FieldType.Keyword)
+    @ApiModelProperty(name = "创建人",example = "klaus")
     private String founder;
+
     @Field(type = FieldType.Keyword)
+    @ApiModelProperty(name = "校长",example = "klaus")
     private String chairman;
 }
--- spring-digger/spring-elasticsearch/src/main/java/com/klaus/interview/springelasticsearch/repository/EsRepo.java
+++ spring-digger/spring-elasticsearch/src/main/java/com/klaus/interview/springelasticsearch/repository/EsRepo.java
@@ -3,7 +3,7 @@ package com.klaus.interview.springelasticsearch.repository;
 import com.klaus.interview.springelasticsearch.model.SchoolPo;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface EsRepo extends ElasticsearchRepository<SchoolPo, Long> {
+public interface EsRepo extends ElasticsearchRepository<SchoolPo, String> {
 
 
 
